c66485c213f2df86f4a1e01fb888ca540f97a89e
atharva naik <atharvanaik2018@gmail.com>
2021-03-11 16:32:33 +0530

    adding emoji removal

--- preprocess.py
+++ preprocess.py
@@ -28,10 +28,15 @@ def remove_urls(text):
     return re.sub(r'http\S+', '', text)
 
 def remove_punctuation(text):
-    for punct in '''"',:`;%@!|\&()#$+-_?<>*=~{}[]''':
+    for punct in '''"',:“”`;%@!|\&()#$+-_?<>*=~{}[]''':
         text = text.replace(punct, " ")
 
     return text
+
+def remove_emoji(text):
+    text = emoji.demojize(text)
+
+    return re.sub(":\S+?:", "", text).strip()
 
 def identity(text):
     return text
@@ -39,6 +44,7 @@ def identity(text):
 VALID_FILTERS = {"lower":lower,  
                  "strip":strip,
                  "remove_urls":remove_urls,
+                 "remove_emoji":remove_emoji,
                  "remove_punctuation":remove_punctuation,
                  "remove_newline":remove_newline,
                  "insert_newline":insert_newline}
@@ -111,12 +117,12 @@ def process_dataset(path, pipeline, seed, valid_size=1/8, test_size=1/8, target=
 if __name__ == "__main__":
     df = pd.read_excel("/home/atharva/interiit/HeadlineGen/raw_data/Development Data/dev_data_article.xlsx")
     print(df.head())
-    pipeline = ["strip", "remove_newline", "remove_url", "remove_punctuation", "insert_newline", "lower"]
-    # pipeline = ["identity"]
+    pipeline = ["strip", "remove_emoji", "remove_newline", "remove_url", "remove_punctuation", "insert_newline", "lower"]
     train, val, test = process_dataset("/home/atharva/interiit/HeadlineGen/raw_data/Development Data/dev_data_article.xlsx", pipeline, 69)
     print(train.head())
     print(val.head())
     print(test.head())
+    print(len(test), len(train), len(val))
 # if TRANSLATE:
 #     index = 0
 #     def translate(text):
