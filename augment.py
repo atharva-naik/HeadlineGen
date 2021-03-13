9739a1fa5ee936cc5179aa6d397a2454a2f4b626
atharva naik <atharvanaik2018@gmail.com>
2021-03-13 22:58:16 +0530

    adding augmented data

--- augment.py
+++ augment.py
@@ -4,6 +4,7 @@ import json
 import tqdm
 import fasttext
 import numpy as np
+import pandas as pd
 from google_trans_new import google_translator
 # this code dumps model at your home directory
 HOME = os.getenv("HOME")
@@ -73,11 +74,79 @@ def augmentCSV(path, keep=['text', 'summary'], article_field='text', summary_fie
         article[article_field] = temp
         augmented.append(article)
 
-    json.dump(augmented, open(path.replace(
-        ".csv", ".json").replace(".xlsx", ".json"), "w"), indent=4)
+    json.dump(augmented, open(path.replace(".csv", ".json").replace(".xlsx", ".json"), "w"), indent=4)
 
+def getAllBitSrings(n: int):
+    import math
+    setB = []
+    n = 2**n-1
+
+    length = len(bin(n)[2:])
+
+    for i in range(n+1):
+        setB.append([])
+        for j in bin(i)[2:].rjust(length,'0'):
+            setB[-1].append(int(j))
+
+    return setB
+
+def getMasks(n: int, map_: dict):
+    helper_mask = getAllBitSrings(len(map_))
+    masks = []
+    # print(helper_mask)
+
+    for mask in helper_mask:
+        masks.append([0 for i in range(n)])
+        for j,val in enumerate(mask):
+            if val == 1:
+                # print(n)
+                # print(map_[j])
+                masks[-1][map_[j]] = 1        
+
+    return masks
+
+def augment(path, thresh):
+    import copy
+    datatset = json.load(open(path))
+    num_lines = 0
+    new_articles = [] 
+    new_num_articles = 0
+
+    for article in tqdm.tqdm(datatset):
+        index_map = {}
+        j = 0
+        for i,line in enumerate(article["text"]):
+            if line['lang'] == "__label__en"  and float(line['conf']) >= thresh and j<=5:
+                index_map[j] = i
+                num_lines += 1
+                j += 1
+        # print(len(index_map))
+        new_num_articles += 2**len(index_map)
+
+        for mask in getMasks(len(article["text"]), index_map):
+            lines = []
+
+            for idx, val in enumerate(mask):
+                if val == 1:
+                    try:
+                        # print(idx, index_map[idx])
+                        lines.append(article['text'][idx]['hi_text'])
+                        # print(article['text'][idx]['hi_text'])
+                    except KeyError:
+                        lines.append(article['text'][idx]['text'])
+                else:
+                    lines.append(article['text'][idx]['text'])
+            new_articles.append({"text":"\n".join(lines), "summary":article["summary"]})
+    
+    print(f"predicted growth factor = {2**(num_lines/len(datatset))}")    
+    print(f"actual growth factor = {new_num_articles/len(datatset)}")  
+    print(len(new_articles))
+    # print(len(new_articles))
+    pd.DataFrame(new_articles).to_csv("proc_data/train_augmented.csv", index=False)
+    
 
 if __name__ == "__main__":
-    augmentCSV(TRAIN)
-    augmentCSV(TEST)
-    augmentCSV(VAL)
+    # augmentCSV(TRAIN)
+    # augmentCSV(TEST)
+    # augmentCSV(VAL)
+    augment("proc_data/train_augmented_article_208_done.json", 0.97) 
